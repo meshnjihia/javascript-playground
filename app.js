9f0d51aae65ca730364e5b38ba109117205f9fe3
meshnjihia <meshworks7@gmail.com>
2023-04-07 11:03:44 +0300

    changes to lesson 2

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-window.onload = () => {
-    console.log('This is your first program executing!');
-}
+// window.onload = () => {
+//     console.log('This is your first program executing!');
+// }
 
-const para = document.querySelector('.dancerStyle');
-para.addEventListener('click', updateName);
+// const para = document.querySelector('.dancerStyle');
+// para.addEventListener('click', updateName);
 
-function updateName() {
-    let name = prompt('Please enter a new name');
-    para.textContent = 'Dancer 1: ' + name;
-} 
+// function updateName() {
+//     let name = prompt('Please enter a new name');
+//     para.textContent = 'Dancer 1: ' + name;
+// } 
+// // this a function to add content to the html doy
+// const myPara = document.getElementById('clickMe');
+// myPara.innerHTML = 'I like ' + 'ba' + (+ 'javascript') + 'a';
+
+// console messages
+// console.log("my name is Meshack")
+// console.log("05/10")
+// console.log("simba the dog")
+// console.log("Venice")

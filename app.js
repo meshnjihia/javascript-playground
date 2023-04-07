d6566d2c64ce38b51649e935957a52a1e7937bfa
meshnjihia <meshworks7@gmail.com>
2023-04-07 09:49:15 +0300

    add some functions to the js file and the html file

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 window.onload = () => {
     console.log('This is your first program executing!');
 }
+
+const para = document.querySelector('.dancerStyle');
+para.addEventListener('click', updateName);
+
+function updateName() {
+    let name = prompt('Please enter a new name');
+    para.textContent = 'Dancer 1: ' + name;
+} 

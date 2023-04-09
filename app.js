d19a10aef2ea121a613087bb41a5e1946562a9ae
meshnjihia <meshworks7@gmail.com>
2023-04-09 08:17:10 +0300

    lesson 4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // function updateName() {
 //     let name = prompt('Please enter a new name');
 //     para.textContent = 'Dancer 1: ' + name;
-// } 
+// }
 // // this a function to add content to the html doy
 // const myPara = document.getElementById('clickMe');
 // myPara.innerHTML = 'I like ' + 'ba' + (+ 'javascript') + 'a';
@@ -18,3 +18,20 @@
 // console.log("05/10")
 // console.log("simba the dog")
 // console.log("Venice")
+
+// lesson 3
+// strings
+console.log('strings'.slice(4).toUpperCase())
+console.log(Number.MAX_SAFE_INTEGER.toLocaleString())
+console.log(Number.MAX_VALUE)
+// boolean
+console.log(true)
+console.log(false)
+console.log(3 < 2)
+
+// undefined and null
+console.log(undefined)
+console.log(null)
+
+// symbols
+console.log(Symbol('symbol'))

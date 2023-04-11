490839788fb296c12dc1af66a66974c8826632c5
meshnjihia <meshworks7@gmail.com>
2023-04-11 10:03:21 +0300

    loops

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 // let orders = 0;
 
 // orders++;
@@ -9,7 +9,11 @@
 // logical operators
 // AND operator
 // const test = "Logical operator";
-// console.log(typeof test === "string" && test.length >= 5)
+// console.log(false && true);
+// const isWarm = true; //
+// const isRaining = false;
+
+// console.log(isWarm && !isRaining);
 
 // // OR
 
@@ -17,3 +21,77 @@
 
 // console.log(test != test.length)
 
+console.log('conditional statements')
+// if and else
+
+// let time = 23;
+
+// if (time > 9) {
+//     console.log("what do you want to do?")
+// }
+// else {
+//     console.log("what do you want to do now?")
+// }
+
+// time loop if else condition
+
+// if (time < 12) {
+//     console.log("Good Morning!")
+// }
+// else if (time < 17) {
+//     console.log("Good Afternoon!")
+// }
+// else if (time < 22) {
+//     console.log("Good Evening!")
+// }
+// else if (time < 24) {
+//     console.log("Good Night!")
+// }
+// else {
+//     console.log(" Time verification failed")
+// }
+
+// ternary operator
+// const doorOpen = false
+
+// if (doorOpen) {
+//   console.log('kindly close the door')
+// } else {
+//   console.log('Thank you for closing the door')
+// }
+
+// doorOpen
+//   ? console.log('Close the door')
+//   : console.log('Thank you for closing the door')
+// let royaltyPoints = 1;
+// if (royaltyPoints > 1) {
+//     console.log('royalty points awarded')
+// }
+// else {
+//     console.log('royalty will be awarded after shopping')
+// }
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// for loop
+// for (let i = 10; i > 0; i--) {
+//     console.log(`${i} bottles of beer on the wall,
+//      ${i} bottles of beer take on down and pass around
+//      there is ${i - 1} bottles of beer on the wall`)
+// }
+
+// while loop
+
+// let lives = 3;
+// while (lives > 0) {
+//     const result = prompt(` you have ${lives} left. what is 2 +2 = ?`)
+//     if (result !== '4') {
+//         lives -= 1
+//     }
+// }
+// alert('you got busted')
